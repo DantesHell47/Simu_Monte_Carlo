bfb5dacd77cf16597e6714f17e9aba5699cb2374
DantesHell47 <Jefferson.dante47@gmail.com>
2023-07-20 00:13:06 -0300

    my first commit

---
+++ simu_monte_carlo.jl
@@ -0,0 +1,22 @@
+using Plots; gr()
+
+vp = []
+vsim = []
+
+nmax = 100
+
+moeda = ["cara", "coroa"]
+
+for nsim in 1:nmax
+	n = 0
+	for i in 1:nsim
+		position = rand(moeda)
+		if position == "cara"
+			n+=1
+		end
+	end
+	push!(vp, n/nsim)
+	push!(vsim, nsim)
+end
+
+plot(vp, vsim)
