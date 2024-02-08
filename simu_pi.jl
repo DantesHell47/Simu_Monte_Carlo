44e93d97ebed00a94a64dca7bc24237deedf4716
DantesHell47 <Jefferson.dante47@gmail.com>
2024-02-08 15:44:34 -0300

    Código refatorado. Foi aproveitado a função filter para filtrar os pontos dentros e foras do círculo.

--- simu_pi.jl
+++ simu_pi.jl
@@ -1,27 +1,12 @@
 using Plots
 
-function norma_euclediana(vetor)
-    return sqrt(sum(x->x^2, vetor))
-end
-
-distance_to_center(x,y) = (x^2 + y^2)^(1/2)
+norma_euclidiana(vetor) = sqrt(sum(x->x^2, vetor))
 
 function estimativa_pi(n)
-    point_inside = Tuple{Float64,Float64}[]
-    point_outside = Tuple{Float64,Float64}[]
-    x = Float64[rand(-1:1e-16:1) for _ in 1:n]
-    y = Float64[rand(-1:1e-16:1) for _ in 1:n]
-
-    for i in 1:n
-        valor_distancia = distance_to_center(x[i],y[i])
-
-        if valor_distancia ≤ 1
-            push!(point_inside, (x[i], y[i]))
-        else
-            push!(point_outside, (x[i], y[i]))
-        end
-    end
-    return point_inside, point_outside
+    pontos_xy = [(rand(-1:1e-2:1), rand(-1:1e-2:1)) for _ in 1:n]
+    points_inside = filter(x->(norma_euclidiana(x) ≤ 1), pontos_xy)
+    points_outside = filter(x->(norma_euclidiana(x) > 1), pontos_xy)
+    return points_inside, points_outside
 end
 
 pontos_dentro, pontos_fora=estimativa_pi(10_000)
